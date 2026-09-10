44f5dc9f828c0354f0ba0b11e956612ac5c0e36e
Yushtify <yush-public.wrinkly478@passmail.net>
2026-09-10 14:41:18 +0300

    Portfoio - v0.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: true,
+    port: 4321, // Astro varsayılan portu, ngrok hangi portu dinliyorsa o olmalı
+  },
+  vite: {
+    plugins: [/** @type {any} */ (tailwindcss())],
+  },
+  devToolbar: { enabled: false },
+});
